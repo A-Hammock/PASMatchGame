882f6dfa03400b67983776449e6bd38127cdee4f
A. Hamm <102124298+A-Hammock@users.noreply.github.com>
2024-01-05 16:13:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function flipCard({target: clickedCard}) {
 function matchCards(img1, img2) {
     if(img1 === img2) {
         matched++;
-        if(matched == 9) {
+        if(matched == 8) {
             setTimeout(() => {
                 return shuffleCard();
             }, 1000);
@@ -48,7 +48,7 @@ function shuffleCard() {
     matched = 0;
     disableDeck = false;
     cardOne = cardTwo = "";
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
     arr.sort(() => Math.random() > 0.5 ? 1 : -1);
     cards.forEach((card, i) => {
         card.classList.remove("flip");
